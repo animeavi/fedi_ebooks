c5e1702de1fd48efa772c0d5936100782891130b
Zero <zero@cock.li>
2021-04-29 14:29:48 -0400

    Make JSON to TXT utility better(?)

--- util/fedi_archive_to_txt.rb
+++ util/fedi_archive_to_txt.rb
@@ -10,6 +10,11 @@ statuses = data['statuses']
 
 corpus = File.new("corpus.txt", "w:UTF-8")
 
+# Enable if you want the archive to have HTML line breaks
+# You do have to make the bot post with HTML (only Pleroma supports it?)
+html_linebreaks = false
+LINEBREAK_PLACEHOLDER = "____LLLLINNEE___BBBBREAKER____" # lol
+
 def filter(content)
   content = content.gsub(/\B[@]\S+\b/, '') || content
 
@@ -25,7 +30,17 @@ end
 statuses.each do |s|
   if s['reblog'].nil?
     content = s['content']
-    content = content.gsub("<br/>", " ")
+
+    # Try to remove line breaks at the start of the post
+    10.times do
+      content = content.strip.gsub(/^<p>/, "") || content
+      content = content.strip.gsub(/^<br\/>/, "") || content
+    end
+
+    content = content.gsub("<br/>", " ") unless html_linebreaks
+    content = content.gsub("<p>", " ") unless html_linebreaks
+    content = content.gsub("<br/>", " #{LINEBREAK_PLACEHOLDER} ") if html_linebreaks
+    content = content.gsub("<p>", " #{LINEBREAK_PLACEHOLDER} ") if html_linebreaks
     content = content.gsub(/<("[^"]*"|'[^']*'|[^'">])*>/, '') # Remove HTML
     content = HTMLEntities.new.decode content.gsub('“', '"').gsub('”', '"').gsub('’', "'").gsub('…', '...')
 
@@ -37,8 +52,33 @@ statuses.each do |s|
       content = filter(content)
     end
 
-    # I don't remember why I have to call it twice
     content = filter(content)
+
+    if html_linebreaks
+      # Try to remove line breaks at the start of the post (again)
+      10.times do
+        content = content.strip.gsub(/^#{LINEBREAK_PLACEHOLDER}/, "") || content
+      end
+
+      content = content.gsub("#{LINEBREAK_PLACEHOLDER}", "&#10;")
+
+      r = "&#10;"
+      while content.end_with? r
+        if content == r
+          content = ""
+          break
+        end
+
+        content = content.slice(content.rindex(r), r.size)
+        content = content.strip
+      end
+
+      while content.start_with? r
+        content = content.sub(r, '')
+        content = content.strip
+      end
+    end
+
     if content != ""
       corpus.puts(content)
     end
